e7c0efb02331b85cccc9a11d84017117dbda8719
Tank Palak <tankpalak35@gmail.com>
2024-08-01 17:32:15 +0000

    this the formost basic step for javascript that is variables.

---
+++ 01_basices/variables.js
@@ -0,0 +1,14 @@
+
+/*
+variables:
+const-for constant 
+var- not recomended because of block scope issue
+let - no problem of bleck scope 
+*/
+
+const accountId=1172454363987
+let accountCity="surat"
+var accountemail="osdfrgbovkm"
+let accountState;
+
+console.table([accountId,accountCity,accountemail,accountState])
