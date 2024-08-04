7a88a7bd4f1c709dbe9cbc7bd44fe555cefa61ca
Tank Palak <tankpalak35@gmail.com>
2024-08-04 14:11:47 +0000

    simple operation and conversion

---
+++ 01_basices/operations.js
@@ -0,0 +1,27 @@
+let value= 3
+let negvalue= -value
+
+console.log(value)
+console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+
+//prefix increment and postfix increment
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
