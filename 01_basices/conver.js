4ee0ec90525a1246514e93867cf4dec5f0b9cb55
Tank Palak <tankpalak35@gmail.com>
2024-08-04 13:42:42 +0000

    basic conversion

---
+++ 01_basices/conver.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+console.log(typeof score)
+
+let stringscore = Number(score)
+
+console.log(typeof stringscore)
+console.log( stringscore)
+
+//"33" to 33
+//"33" to true/false boolean
+//null will give nan
+//undefined will give nan
+
+let isLoggedin = null
+
+console.log(typeof isLoggedin)
+
+let numberisloggedin = String(score)
+
+console.log(typeof numberisloggedin)
+console.log( numberisloggedin)
+
+//palak to nan in number
+//palak to true in boolene
+//null to nan
+//null to undefined in string
